b8f3184515b478fcf517d878933b58dd7fbaff8f
jewoodev <jewoos15@naver.com>
2026-02-06 17:26:34 +0900

    refactor: 순수한 도메인을 위해 잘못 스며든 '명령' 의존성을 애플리케이션 하위로 이동

--- src/main/kotlin/data/spring/mybatis/application/provided/product/ProductUseCase.kt
+++ src/main/kotlin/data/spring/mybatis/application/provided/product/ProductUseCase.kt
@@ -2,16 +2,15 @@ package data.spring.mybatis.application.provided.product
 
 import data.spring.mybatis.application.service.product.command.ProductSearchCommand
 import data.spring.mybatis.domain.product.Product
-import data.spring.mybatis.domain.product.request.ProductUpdateCommand
+import data.spring.mybatis.application.service.product.command.ProductUpdateCommand
 
 interface ProductUseCase {
-    fun save(product: Product)
-    fun saveAll(products: List<Product>): Int
+    fun save(product: Product): Int
+
+    fun update(updateCommand: ProductUpdateCommand): Int
+
+    fun deleteAll(): Int
 
     fun findById(productId: Long): Product?
     fun findWithCond(searchCommand: ProductSearchCommand): List<Product>
-
-    fun update(updateCommands: List<ProductUpdateCommand>): Int
-
-    fun deleteAll(): Int
 }
